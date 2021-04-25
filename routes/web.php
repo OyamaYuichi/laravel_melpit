7f666839351f96c9f98fc928fb825581193bfd94
OyamaYuichi <yuichi19991208@gmail.com>
2021-04-25 14:51:02 +0000

    商品出品画面の作成

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,8 @@ Route::prefix('mypage')
     ->group(function () {
         Route::get('edit-profile', 'ProfileController@showProfileEditForm')->name('mypage.edit-profile');
         Route::post('edit-profile', 'ProfileController@editProfile')->name('mypage.edit-profile');
+        Route::get('sold-items', 'SoldItemsController@showSoldItems')->name('mypage.sold-items');
+        Route::get('items/{item}', function () {return "商品詳細";})->name('item');
     });
 
 Auth::routes();
