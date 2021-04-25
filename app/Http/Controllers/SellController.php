0dce644781046148bcfc54b7ac89303868e4dafc
OyamaYuichi <yuichi19991208@gmail.com>
2021-04-25 13:41:45 +0000

    商品の状態を実装

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ItemCondition;
 
 class SellController extends Controller
 {
     public function showSellForm()
     {
-        return view('sell');
+        $conditions = ItemCondition::orderBy('sort_no')->get();
+        return view('sell')
+            ->with('conditions', $conditions);
     }
 }
