899736b3f793589f547325f634070121ebe9299d
yuichi oyama <yuichi19991208@gmail.com>
2021-04-25 10:37:14 +0900

    会員登録画面の作成

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('top');
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+
