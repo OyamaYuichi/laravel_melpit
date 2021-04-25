ecf55f826a09ca1750ce0c02dcf576ee98de98a7
OyamaYuichi <yuichi19991208@gmail.com>
2021-04-25 13:56:13 +0000

    カテゴリを実装

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ItemCondition;
+use App\Models\PrimaryCategory;
 
 class SellController extends Controller
 {
     public function showSellForm()
     {
+        $categories = PrimaryCategory::orderBy('sort_no')->get();
         $conditions = ItemCondition::orderBy('sort_no')->get();
         return view('sell')
+            ->with('categories', $categories)
             ->with('conditions', $conditions);
     }
 }
